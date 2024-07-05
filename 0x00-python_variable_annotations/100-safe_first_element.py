a9d80ab5f6474faa8f22d83b557d77db22fbb6f3
AshTechs <ashbelh@gmail.com>
2024-07-04 19:55:29 -0700

    Corrected

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,20 +3,20 @@
 Module for safely retrieving the first element from a list.
 """
 
-from typing import Sequence, Union
+from typing import Sequence, TypeVar, Union
+
+T = TypeVar('T')
 
 
-def safe_first_element(lst: Sequence) -> Union[Sequence, None]:
+def safe_first_element(lst: Sequence[T]) -> Union[T, None]:
     """
     Safely retrieves the first element from a list.
 
     Args:
-        lst (Sequence): The input list.
+        lst (Sequence[T]): The input list.
 
     Returns:
-        Union[Sequence, None]:
-        - The first element of the list,
-        - None if the list is empty.
+        Union: The first element of the list, or None if the list is empty.
     """
     if lst:
         return lst[0]
