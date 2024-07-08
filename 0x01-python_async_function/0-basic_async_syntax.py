404bb13be47ecef4f44f72f6dda7a09404a822c1
AshTechs <ashbelh@gmail.com>
2024-07-08 15:22:26 -0700

    Done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module contains asynchronous coroutine that waits for a random delay.
+This module contains an asynchronous coroutine that waits for a random delay
+between 0 and a specified max_delay (inclusive) seconds and returns the delay.
 """
 
 import asyncio
@@ -8,7 +9,7 @@ import random
 from typing import Union
 
 
-async def wait_random(max_delay: int = 10) -> Union[int, float]:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0 and max_delay (inclusive) seconds.
 
