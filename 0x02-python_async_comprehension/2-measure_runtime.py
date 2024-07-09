c0d9bcd839a9d79717dd4af8d96d33055742c3f5
AshTechs <ashbelh@gmail.com>
2024-07-09 13:49:21 -0700

    Executable

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,18 +2,13 @@
 """
 Module to measure the runtime of async_comprehension executed in parallel.
 """
+
 import asyncio
 from typing import List
 from time import time
 
 
 async def measure_runtime() -> float:
-    """
-    Measures the total runtime of executing async_comprehension four times in parallel.
-
-    Returns:
-        float: Total runtime in seconds.
-    """
     start_time = time()
 
     tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
