fe8d679e5f85bc629e61f428899d6dcf090c6ae9
AshTechs <ashbelh@gmail.com>
2024-07-09 08:55:55 -0700

    Corrected

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,44 +1,27 @@
 #!/usr/bin/env python3
 """
-This contains asynchronous coroutine `wait_n` that spawns `wait_random` n times
-with a specified max_delay and returns the list of delays in ascending order.
+This module contains an asynchronous coroutine that spawns multiple instances
+of wait_random and returns a list of delays in ascending order.
 """
 
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+from 0_basic_async_syntax import wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns `wait_random` n times with the specified max_delay.
+    Spawns `n` instances of wait_random with a specified `max_delay` and rtrns
+    a list of delays in ascending order.
 
     Args:
-        n (int): The number of times to spawn `wait_random`.
-        max_delay (int): The maximum number of seconds to wait for each delay.
+        n (int): Number of times to call wait_random.
+        max_delay (int): The maximum number of seconds to wait in each call.
 
     Returns:
-        List[float]: A list of all the delays (float values) in ascending order
+        List[float]: A sorted list of delays (float values).
     """
-    async def run_task(max_delay: int) -> float:
-        return await wait_random(max_delay)
-
-    tasks = [run_task(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-
-    sorted_delays = []
-    while delays:
-        min_delay = min(delays)
-        delays.remove(min_delay)
-        sorted_delays.append(min_delay)
-
-    return sorted_delays
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    # Example usage
-    print(asyncio.run(wait_n(5, 5)))
-    print(asyncio.run(wait_n(10, 7)))
-    print(asyncio.run(wait_n(10, 0)))
+    tasks: List[asyncio.Task] =
+    [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays: List[float] = [await task for task in asyncio.as_completed(tasks)]
+    return delays
