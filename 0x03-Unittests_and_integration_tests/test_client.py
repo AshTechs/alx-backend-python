c75e0e99b9eff280199cd9fdafed83612af69c61
AshTechs <ashbelh@gmail.com>
2024-07-29 17:43:32 -0700

    Corrected

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,14 +27,18 @@ class TestGithubOrgClient(unittest.TestCase):
 
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, expected_response)
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
 
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org):
         """
         Test that GithubOrgClient._public_repos_url returns the correct value.
         """
-        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
+        mock_org.return_value = {
+            "repos_url": "https://api.github.com/orgs/test_org/repos"
+        }
 
         client = GithubOrgClient("test_org")
         expected_url = "https://api.github.com/orgs/test_org/repos"
@@ -43,7 +47,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
         """
-        Test that GithubOrgClient.public_repos returns the correct list of repositories.
+        Tests GithubOrgClient.public_repos returns the correct list of repos.
         """
         test_payload = [
             {"name": "repo1"},
@@ -53,7 +57,8 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = test_payload
         test_url = "https://api.github.com/orgs/test_org/repos"
 
-        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
             mock_public_repos_url.return_value = test_url
 
             client = GithubOrgClient("test_org")
